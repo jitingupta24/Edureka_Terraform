945332823ac4db78943de0c3b1d2bc049c8eece9
jitingupta24 <63880919+jitingupta24@users.noreply.github.com>
2023-06-18 01:36:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+}
 
+variable "ami" {
+  description = "ID of AMI to use for the instance"
+  type        = string
+  default	  = "ami-024e6efaf93d85776"
+}
