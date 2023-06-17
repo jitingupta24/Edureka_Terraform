49cfe9c4df1fe4ea0ad8112a75787698705e5adc
jitingupta24 <63880919+jitingupta24@users.noreply.github.com>
2023-06-17 23:30:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+    access_key = "AKIAYX5CMQUBV4J6YTZQ"
+    secret_key = "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"
+    region = "us-east-1"
+      
+}
+
+resource "aws_instance" "my-vm" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-01e9e2afe946219ef"]
+    subnet_id = "subnet-01b0d89c1e613e35b"
+
+user_data = <<-EOF
+#!/bash/bin
+apt-get update -y
+sudo apt-get install nginx -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
+EOF
+
+    tags = {
+      Name = "TF_VM"
+    }
+  
+}
