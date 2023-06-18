d0748ea0511413e66d71ef1e3c3def039ff78cc1
jitingupta24 <63880919+jitingupta24@users.noreply.github.com>
2023-06-18 09:52:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,15 @@ variable "ami" {
   type        = string
   default	  = "ami-024e6efaf93d85776"
 }
+
+variable "access_key" {
+  description = "access key"
+  type        = string
+
+}
+
+variable "secret_key" {
+  description = "aecret Key"
+  type        = string
+  
+}
